1c9d0f497004a97b3d3c5f3f74e317ca043223c8
THREEhundo <samuelbaik87@gmail.com>
2023-03-16 19:45:53 -0700

    redone cards

--- app/search/[query]/page.js
+++ app/search/[query]/page.js
@@ -21,8 +21,6 @@ const SearchPage = ({ params }) => {
 		}
 		fetchData()
 	}, [query])
-	//const { search } = params
-	//const [results, setResults] = useState([])
 	const imagePath = 'https://image.tmdb.org/t/p/original/'
 
 	useEffect(() => {
@@ -42,64 +40,52 @@ const SearchPage = ({ params }) => {
 	}, [])
 
 	return (
-		<div className=' mx-16 md:mx-32 lg:mx-48 mb-12'>
-			<div>
-				Search Page
-				{searchResults.map(result => (
-					<div key={result.id}>
-						{/*<Link
-						key={result.id}
-						className=''
-						href={`/movie/${result.id}`}>*/}
-						<figure className='flex flex-col md:flex-row items-center bg-slate-100 rounded-xl p-0 dark:bg-slate-800 '>
+		<div className=' mx-16 md:mx-32 lg:mx-48 mb-12 flex flex-col items-center'>
+			Results
+			{searchResults.map(
+				result =>
+					result.poster_path && (
+						<Link
+							key={result.id}
+							href={`/movie/${result.id}`}
+							className='flex flex-col items-center  border rounded-lg shadow md:flex-row md:max-w-xl  border-gray-700 bg-gray-800 hover:bg-gray-700 my-3'>
 							<Image
-								className='w-[33.33%] object-cover md:mr-5 h-full rounded-none mx-auto'
+								className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
 								src={imagePath + result.poster_path}
 								alt={result.title || result.name}
 								width={384}
 								height={576}
 							/>
-							<div className='p-6 md:p-8 text-center md:text-left space-y-4'>
-								<figcaption className='font-medium'>
-									<div className='text-sky-500 dark:text-sky-400'>
-										<Link href={`/movie/${result.id}`}>
-											{result.title || result.name}
-										</Link>
-									</div>
-									<div className='text-slate-700 dark:text-slate-500'>
-										{result.release_date}
-									</div>
-								</figcaption>
-								<blockquote>
-									<p className='text-sm font-small'>
-										{!isTablet ? (
-											<ShortenedText
-												text={result.overview}
-												maxLength={200}
-												linkText='...Read More'
-												linkUrl={`/movie/${result.id}`}
-											/>
-										) : (
-											<ShortenedText
-												text={result.overview}
-												maxLength={400}
-												linkText='...Read More'
-												linkUrl={`/movie/${result.id}`}
-											/>
-										)}
-									</p>
-								</blockquote>
+							<div className='flex flex-col justify-between p-4 leading-normal'>
+								<h5 className='mb-2 text-2xl font-bold tracking-tight text-white'>
+									{result.title || result.name}
+								</h5>
+								<p className='mb-3 font-normal text-gray-400'>
+									{!isTablet ? (
+										<ShortenedText
+											text={result.overview}
+											maxLength={200}
+											linkText='...Read More'
+											linkUrl={`/movie/${result.id}`}
+										/>
+									) : (
+										<ShortenedText
+											text={result.overview}
+											maxLength={400}
+											linkText='...Read More'
+											linkUrl={`/movie/${result.id}`}
+										/>
+									)}
+								</p>
 							</div>
-						</figure>
-						{/*</Link>*/}
-					</div>
-				))}
-			</div>
+						</Link>
+					)
+			)}
 		</div>
 	)
 }
 
-const ShortenedText = ({ text, maxLength, linkText, linkUrl }) => {
+const ShortenedText = ({ text, maxLength, linkText }) => {
 	if (text.length <= maxLength) {
 		return text
 	}
@@ -110,14 +96,10 @@ const ShortenedText = ({ text, maxLength, linkText, linkUrl }) => {
 		0,
 		lastSpaceIndex
 	)
-	const link = (
-		<Link className='text-blue-600' href={linkUrl}>
-			{linkText}
-		</Link>
-	)
+	const link = <span className='text-blue-600'>{linkText}</span>
 
 	return (
-		<span className='text-sm font-medium'>
+		<span>
 			{shortenedTextWithoutWordBreak} {link}
 		</span>
 	)
