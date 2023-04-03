33eda7b272abaa32e4585bea50be9ffe0fddeab4
THREEhundo <samuelbaik87@gmail.com>
2023-04-02 20:52:36 -0700

    commented out

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,11 +13,11 @@ export default async function auth(req, res) {
 		],
 
 		pages: {
-			signIn: '/auth/signin',
-			signOut: '/auth/signout',
-			error: '/auth/error', // Error code passed in query string as ?error=
-			verifyRequest: '/auth/verify-request', // (used for check email message)
-			newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+			//signIn: '/auth/signin',
+			//signOut: '/auth/signout',
+			//error: '/auth/error', // Error code passed in query string as ?error=
+			//verifyRequest: '/auth/verify-request', // (used for check email message)
+			//newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
 		},
 
 		adapter: PrismaAdapter(prisma),
